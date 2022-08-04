848c141b26ec67d99c7dd753c2395a29d65e8662
sweetcapucino <thuylinh271055@gmail.com>
2022-08-04 16:26:59 +0700

    feat: store states to localStorage (redux-persist)

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 
 import App from './App';
-import store from './reducers/store';
+import store, { persistor } from "./reducers/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
+
   </Provider>
 );
 
--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,16 +1,28 @@
 import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import todoReducer from "./todo";
-import authReducer from "./login"
+import authReducer from "./login";
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    // whitelist: "auth"
+}
 
 const reducer = combineReducers({
     todo: todoReducer,
     auth: authReducer
 })
 
+const persistedReducer = persistReducer(persistConfig, reducer)
+
 const store = configureStore({
-    reducer
+    reducer: persistedReducer
 })
+
+export const persistor = persistStore(store);
 
 export default store
